ef038322e683b8ad3cb024b3e958b5dc2a513ba3
Ryan Garren <rryangarren@gmail.com>
2021-03-02 16:51:50 -0700

    rev 1

---
+++ src/components/Topics/EvenAndOdd.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class EvenAndOdd extends Component {
+    render() {
+        return (
+            <div className = 'puzzleBox evenAndOddPB'>
+                <h4> Evens and Odds </h4>
+
+
+
+            </div>
+        )
+    }
+}
+
+export default EvenAndOdd;
